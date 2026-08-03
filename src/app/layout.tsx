21f6a2369b5c646e4e83640b0772ce2d10082a54
sahil-codex <sahil2flamingo@gmail.com>
2026-08-03 18:24:56 +0530

    adding favican and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,20 @@
 import { Nunito } from "next/font/google";
+import type {Metadata} from "next";
 import "./globals.css";
 const nunito = Nunito({
   subsets: ["latin"],
 });
 
+
+export const metadata: Metadata = {
+  title: "Cozy-Memory",
+  description: "Cozy-Memory",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
